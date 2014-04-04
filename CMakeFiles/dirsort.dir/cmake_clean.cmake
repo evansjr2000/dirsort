ec67c8cfa3d33ab02c413c16f32ceec07545606e
evansjr <evansjr@paratus.sd.spawar.navy.mil>
2014-04-04 14:23:51 -0700

    Initial commit of dirsort source code.

---
+++ CMakeFiles/dirsort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "dirsort.c"
+  "CMakeFiles/dirsort.dir/dirsort.c.o"
+  "dirsort.pdb"
+  "dirsort"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/dirsort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
